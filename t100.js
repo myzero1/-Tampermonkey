16b0c7d47f82c32f714810536c4aac5181360196
myzero1 <myzero1@sina.com>
2017-09-29 10:34:15 +0800

    v1.0finsh
    swich tab and page

--- t100.js
+++ t100.js
@@ -8,12 +8,29 @@
 // @match        https://www.baidu.com/?to_top=1
 // @grant GM_setValue
 // @grant GM_getValue
+// @grant GM_listValues
+// @grant GM_log
+// @grant GM_deleteValue
 // @require    http://libs.baidu.com/jquery/1.9.0/jquery.min.js
 // ==/UserScript==
 
 //---------------------
 $(document).ready(function(){
+    //deal_gm();return false;
+
     go_top();
+
+    //----------deal GM-----------------
+    function deal_gm(){
+        GM_log(GM_listValues());
+        var aGmName = GM_listValues();
+        for( var i=0;i<aGmName.length;i++) {
+            var sKey = aGmName[i];
+            console.log(sKey+':'+GM_getValue(sKey));
+            GM_deleteValue(sKey);
+        }
+        return false;
+    }
 
     //---------to top--------------
     function go_top(){
@@ -21,12 +38,14 @@ $(document).ready(function(){
         var index_url = 'https://www.baidu.com/?to_top=1';
         if (window.location.href==index_url) {
             if (get_last_file_name()!=false) {
-                var sHtmlA = '<a id="go_top" target="_self" href="https://top.taobao.com/index.php?topId=TR_FS&leafId=50012027&rank=search&type=hot&s=0">to top</a>';
+                var sHtmlA = '<a id="go_top" target="_self" href="https://top.taobao.com/index.php?spm=a1z5i.3.3.34.tjDtln&topId=TR_FS&leafId=50012027">to top</a>';
                 $("#lg").before(sHtmlA);
                 $('#go_top')[0].click();
             }
         } else {
-            main();
+            if (get_last_file_name()!=false) {
+                main();
+            }
         }
     }
 
@@ -40,6 +59,7 @@ $(document).ready(function(){
         //------to save---------
 
         if (is_end_page()) {
+            var sFileName = GM_getValue("sLastFileName","");
             save_to_file(sFileName);
         } else {
 
@@ -55,19 +75,24 @@ $(document).ready(function(){
         var year = myDate.getFullYear();    //获取完整的年份(4位,1970-????)
         var month = 1+myDate.getMonth();       //获取当前月份(0-11,0代表1月)
         var date = myDate.getDate();        //获取当前日(1-31)
-        var sDate = year+'-'+month+'-'+date+'.csv';
+        var rank = getUrlParam('rank')==null ? 'sale' : getUrlParam('rank');
+        var type = getUrlParam('type')==null ? 'up' : getUrlParam('type');
+        var sDate = rank+'_'+type+'_'+year+'-'+month+'-'+date+'.csv';
+
         var sFileName = GM_getValue("sLastFileName","");
+        var sTabName = rank+'_'+type;
+        var sYesTodayLastFileName = GM_getValue(sTabName,"");
         if (sFileName=="") {
             sFileName = sDate;
         } else {
-            if (sFileName==sDate) {
+            if (sFileName==sYesTodayLastFileName) {
                 return false;
             } else {
                 sFileName = sDate;
             }
         }
 
-        GM_setValue("sLastFileName",sDate);
+        GM_setValue("sLastFileName",sFileName);
     }
 
     //---------is end page--------------
@@ -87,13 +112,37 @@ $(document).ready(function(){
         console.log('save_to_file');
         var str = GM_getValue("sRows","");
         str =  encodeURIComponent(str);
-        var oDownloadA = $('.switch-item').last().find('a');
+        var oDownloadA = $('.switch-item').first().find('a');
         oDownloadA.attr('download', sFileName);
         oDownloadA.attr('href', "data:text/csv;charset=utf-8,"+str);
         oDownloadA[0].click();
         GM_setValue("sRows","");
         GM_setValue("sLastFileName",sFileName);
+
+        var myDate = new Date();
+        var year = myDate.getFullYear();    //获取完整的年份(4位,1970-????)
+        var month = 1+myDate.getMonth();       //获取当前月份(0-11,0代表1月)
+        var date = myDate.getDate();        //获取当前日(1-31)
+        var rank = getUrlParam('rank')==null ? 'sale' : getUrlParam('rank');
+        var type = getUrlParam('type')==null ? 'up' : getUrlParam('type');
+        var sDate = rank+'_'+type+'_'+year+'-'+month+'-'+date+'.csv';
+        var sTabName = rank+'_'+type;
+        GM_setValue(sTabName,sDate);
+        //GM_log(GM_listValues());return false;
+
+        switch_tab();
     }
+
+    //-----------switch_tab--------------
+    function switch_tab(){
+        var has_next = $('.switch-item-selected').next().length;
+        if (has_next==1) {
+            $('.switch-item-selected').next().find('a')[0].click();
+        } else {
+            //GM_setValue("sYesTodayLastFileName",GM_getValue("sLastFileName"));
+        }
+    }
+
 
 
     //--------get mulit page-----
@@ -142,5 +191,12 @@ $(document).ready(function(){
     function trim(str)
          {
              return str.replace(/(^\s*)|(\s*$)/g, "");
+     }
+
+     function getUrlParam (name) {
+          var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+          var r = window.location.search.substr(1).match(reg);
+         //console.log(window.location.search);return false;
+          if (r != null) return unescape(r[2]); return null;
      }
 });
